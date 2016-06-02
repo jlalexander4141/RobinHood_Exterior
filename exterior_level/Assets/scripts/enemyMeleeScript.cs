c8863f610f78e9d7ee305c8a8b43dc43e49cd943
jlalexander4141 <jlalexander4141@gmail.com>
2016-06-02 17:14:15 -0500

    v12
    submission for 1304 secondary

---
+++ exterior_level/Assets/scripts/enemyMeleeScript.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class enemyMeleeScript : MonoBehaviour {
+
+	//variables
+	public Transform myTarget;
+	public float attackDistance;
+	public float hitRate;
+	private float hitTime = 0.0f;
+
+	public float enemyHealth, minHealth, maxHealth;
+
+	//collision function
+	void OnTriggerEnter(Collider other){
+
+		if (other.tag == "PlayerBullet") {
+
+			enemyHealth--;
+
+			Destroy (other.gameObject);
+
+		}
+
+	}
+
+	// Use this for initialization
+	void Start () {
+
+		enemyHealth = Random.Range (minHealth, maxHealth);
+
+		myTarget = GameObject.FindWithTag ("Player").transform;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		float dist = Vector3.Distance (myTarget.position, transform.position);
+
+		if (dist <= attackDistance) {
+
+			if(Time.time > hitTime){
+
+				transform.LookAt (myTarget);
+
+
+				hitTime = Time.time + hitRate;
+
+			}
+
+		}
+
+		//kill enemy
+		if (enemyHealth <= 0.0f){
+
+			Destroy (gameObject);
+
+		}
+	
+	}
+}
